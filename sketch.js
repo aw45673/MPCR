2a651500a021c176640814adcf8d0789beaf2990
MyRecipes <Agwingler@yahoo.com>
2021-01-18 21:27:13 -0500

    MC

--- sketch.js
+++ sketch.js
@@ -12,11 +12,11 @@ var cars, car1, car2, car3, car4;
 var car1Image,car2Image,car3Image,car4Image,trackImage;
 
 function preload(){
-  car1Image=loadImage("../images/car1.png")
-  car2Image=loadImage("../images/car2.png")
-  car3Image=loadImage("../images/car3.png")
-  car4Image=loadImage("../images/car4.png")
-  trackImage=loadImage("../images/track.jpg")
+  car1Image=loadImage("images/car1.png")
+  car2Image=loadImage("images/car2.png")
+  car3Image=loadImage("images/car3.png")
+  car4Image=loadImage("images/car4.png")
+  trackImage=loadImage("images/track.jpg")
 }
 
 function setup(){
